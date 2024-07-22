048b299f111d41f5dd28bf3db3a23ca5665a5b52
DevOpsTrainerNew <gcptrainer4@gmail.com>
2024-07-22 09:33:16 +0530

    added new file calc.py

---
+++ calc.py
@@ -0,0 +1,15 @@
+def add(num1, num2):
+    sum = num1 + num2
+    return sum
+
+def sub(num1, num2):
+    minus = num1 - num2
+    return minus
+
+def mul(num1, num2):
+    cross = num1 * num2
+    return cross
+
+def div(num1, num2):
+    divid = num1 / num2
+    return divid
